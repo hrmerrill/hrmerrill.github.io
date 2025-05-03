7a30c2d15e0e41c1c020e9fffaa35486824cd6ef
Hunter R. Merrill <hunter.merrill@mac.lan>
2025-05-03 12:36:55 -0400

    some animation, for fun

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function detectNarrow() {
     return isNarrow1 || isNarrow2 || isNarrow3;
 };
 
+// rearrange cards based on screen size
 function toggleCards() {
     const isNarrow = detectNarrow();
     if (isNarrow) {
@@ -29,6 +30,52 @@ function toggleCards() {
         education.classList.remove("w3-margin-bottom");
     }
 };
+
+// Add movement animation to cards when they come into view
+let options = { root: null, rootMargin: '0px', threshold: 0.01 };
+function bottomsUpCallbackFunc(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // by ID, so that only the elements I name get the animation
+            var textId = entry.target.id;
+            if (textId) {
+                var elem = document.getElementById(entry.target.id);
+                try {
+                    elem.classList.add("w3-animate-bottom");
+                } catch (e) {
+                    console.log("Error adding class to element:", e);
+                }
+            }
+        }
+    })
+}
+let bottomsUpObserver = new IntersectionObserver(bottomsUpCallbackFunc, options);
+document.querySelectorAll(".w3-container, .w3-display-container").forEach(card => {
+    bottomsUpObserver.observe(card);
+});
+
+// Add color transition to tags when they come into view
+function colorTransitionCallbackFunc(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            if (entry.target.classList.contains("w3-blue")) {
+                entry.target.classList.add("w3-animate-pill-dark-shimmer");
+            } else if (entry.target.classList.contains("w3-light-blue")) {
+                entry.target.classList.add("w3-animate-pill-medium-shimmer");
+            } else if (entry.target.classList.contains("w3-very-light-blue")) {
+                entry.target.classList.add("w3-animate-pill-light-shimmer");
+            }
+        } else {
+            entry.target.classList.remove("w3-animate-pill-dark-shimmer");
+            entry.target.classList.remove("w3-animate-pill-medium-shimmer");
+            entry.target.classList.remove("w3-animate-pill-light-shimmer");
+        }
+    })
+}
+let tagObserver = new IntersectionObserver(colorTransitionCallbackFunc, options);
+document.querySelectorAll(".w3-tag.w3-round-xlarge").forEach(card => {
+    tagObserver.observe(card);
+});
 
 window.addEventListener("resize", toggleCards);
 document.addEventListener("DOMContentLoaded", toggleCards);
